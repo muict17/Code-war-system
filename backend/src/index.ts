8742886355ee75b2dd62e57b69a816dd03f4284c
lagmanzaza <lagmanmc@gmail.com>
2020-01-08 11:34:48 +0700

    [Progress] fix jest not found dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
 import server from "./app";
-require("dotenv").config({ path: "../.env-sample" });
+require("dotenv").config();
 const port = Number(process.env.SERVER_PORT) || 3000;
 server().listen(port, "0.0.0.0");
