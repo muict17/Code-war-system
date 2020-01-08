2116965ecca69ebb91eeab2e2eddbdfe55805a93
lagmanzaza <lagmanmc@gmail.com>
2020-01-08 10:56:23 +0700

    [Fix] .env not found

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
 import server from "./app";
-require("dotenv").config();
+require("dotenv").config({ path: "../.env-sample" });
 const port = Number(process.env.SERVER_PORT) || 3000;
 server().listen(port, "0.0.0.0");
