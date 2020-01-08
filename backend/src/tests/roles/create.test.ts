4644e40ca5b0167ee17dbeb8934d62e0d17f3702
lagmanzaza <lagmanmc@gmail.com>
2020-01-08 11:09:20 +0700

    [Progress] fix jest not found dotenv

--- backend/src/tests/roles/create.test.ts
+++ backend/src/tests/roles/create.test.ts
@@ -1,6 +1,7 @@
 import createRoleService from "../../services/user-roles/create";
 
 test("create user role", async () => {
+  console.log(process.env);
   const result = await createRoleService("test");
   expect(result.name).toEqual("test");
 });
