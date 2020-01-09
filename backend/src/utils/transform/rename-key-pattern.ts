186d099895d09c851e574f0c18bbd7624da28cf3
lagmanzaza <lagmanmc@gmail.com>
2020-01-09 21:01:47 +0700

    [Improvement

--- backend/src/utils/transform/rename-key-pattern.ts
+++ backend/src/utils/transform/rename-key-pattern.ts
@@ -1,12 +1,12 @@
 export default (object: Object, pattern: string): any => {
   return Object.keys(object)
     .filter(key => {
-      return key.search(pattern) !== -1;
+      return key.search(pattern) !== -1 && object[key] !== null;
     })
     .map(key => {
       return { [key.replace(pattern, "")]: object[key] };
     })
     .reduce((result, current) => {
       return Object.assign(result, current);
-    });
+    }, {});
 };
