448eabb45605b354bd231baee61303be31af9e30
Ryan-Willpower <surasak.c.work@gmail.com>
2020-01-10 23:48:43 +0700

    feat: add Font Awesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
 import Vuelidate from "vuelidate";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+
+library.add(faUser);
+
+Vue.component("FontAwesomeIcon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
