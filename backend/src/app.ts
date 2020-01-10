7ecc18504e072b5a8861d89f4b98234334892b13
lagmanzaza <lagmanmc@gmail.com>
2020-01-10 14:18:26 +0700

    [Utility] Function for authorization

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,31 @@
 import * as fastify from "fastify";
 import { Server, IncomingMessage, ServerResponse } from "http";
+import * as helmet from "fastify-helmet";
+import * as compress from "fastify-compress";
+import * as cors from "fastify-cors";
+import * as staticFile from "fastify-static";
+import * as path from "path";
+import multer from "fastify-multer";
 import routes from "./routes";
 import logger from "./logger";
-
+import verifyOwner from "./utils/authorization";
 const server: fastify.FastifyInstance<
   Server,
   IncomingMessage,
   ServerResponse
 > = fastify();
-// { logger: { prettyPrint: true } }
 export default () => {
+  server.register(staticFile, {
+    root: path.join(__dirname, "../files"),
+    prefix: "/public/",
+    decorateReply: false
+  });
+  server.register(multer.contentParser);
+  server.register(helmet, { hidePoweredBy: { setTo: "PHP 4.2.0" } });
+  server.register(compress, { global: false });
+  server.register(cors);
   server.decorateRequest("logger", logger);
+  server.decorateRequest("authorization", verifyOwner);
   routes.forEach((route: any) => {
     server.route(route);
   });
