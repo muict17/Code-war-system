ec58a5f668dbfe4fab36edbabf776efb9da80120
lagmanzaza <lagmanmc@gmail.com>
2020-01-10 16:53:59 +0700

    [API, user] update by id

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,11 @@
 import roleCreateRoute from "./roles/create";
 import userCreateRoute from "./users/create";
 import userGetByIdRoute from "./users/get-id";
+import userUpdateRoute from "./users/update";
 
-export default [roleCreateRoute, userCreateRoute, userGetByIdRoute];
+export default [
+  roleCreateRoute,
+  userCreateRoute,
+  userGetByIdRoute,
+  userUpdateRoute
+];
