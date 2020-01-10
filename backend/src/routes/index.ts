b4064ab0b4a8c9ea01799a55063e2ce5a8eb5568
lagmanzaza <lagmanmc@gmail.com>
2020-01-10 15:18:03 +0700

    [API, users] get user by id

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,4 +1,5 @@
 import roleCreateRoute from "./roles/create";
 import userCreateRoute from "./users/create";
+import userGetByIdRoute from "./users/get-id";
 
-export default [roleCreateRoute, userCreateRoute];
+export default [roleCreateRoute, userCreateRoute, userGetByIdRoute];
---
+++ backend/src/routes/users/get-id/index.ts
@@ -0,0 +1,32 @@
+import schema from "./schema";
+import getUserByIdService from "../../../services/users/get-id";
+
+export default {
+  url: "/users/:userId",
+  method: "GET",
+  schema,
+  preHandler: async (req: any, res: any, done: any) => {
+    await req.authorization.authenticate(req, res);
+    const isOwner = req.authorization.verifyOwner(
+      req.userInfo,
+      req.params.userId
+    );
+    const isRole = req.authorization.verifyRole(req.userInfo, ["admin"]);
+    if (isOwner || isRole) {
+      done();
+      return;
+    }
+    res.status(403).send({ message: "Insufficient Permission" });
+  },
+  handler: async (req: any, res: any) => {
+    try {
+      const result = await getUserByIdService(req.params.userId);
+      res.status(200).send(result);
+      return;
+    } catch (e) {
+      req.logger.error(e);
+      const { code, message } = req.createErrorResponse("User", e.message);
+      res.status(code).send({ message });
+    }
+  }
+};
