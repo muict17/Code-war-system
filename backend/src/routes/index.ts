d6084b6c2289d4631bcd000c074773866d0586ca
lagmanzaza <lagmanmc@gmail.com>
2020-01-10 20:29:48 +0700

    [API, users] verify user by token

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,8 +4,9 @@ import userGetByIdRoute from "./users/get-id";
 import userUpdateRoute from "./users/update";
 import userDeleteRoute from "./users/delete";
 import userGetByQueryRoute from "./users/get-query";
-
+import userVerifyRoute from "./users/verify-user";
 export default [
+  userVerifyRoute,
   userGetByQueryRoute,
   userDeleteRoute,
   roleCreateRoute,
