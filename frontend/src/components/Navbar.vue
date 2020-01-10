3436477dbcf9b0b9830beded1fe38bac6d9daa75
Ryan-Willpower <surasak.c.work@gmail.com>
2020-01-10 23:49:10 +0700

    feat: add navigation component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="navbar">
+    <router-link class="item" to="/">Codewar</router-link>
+    <div class="menus">
+      <div class="item">
+        <FontAwesomeIcon :icon="['far', 'user']" />
+        User: xxxx
+      </div>
+      <router-link class="item" to="/ranking">Ranking</router-link>
+      <div class="item">Logout</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style lang="sass" scoped>
+.navbar, .item, a
+  color: #acadaf
+  font-size: 1rem
+
+
+.navbar, .menus
+  display: flex
+  align-items: center
+
+.navbar
+  margin: 20px 120px
+  justify-content: space-between
+
+.menus > .item
+  margin: 0 10px
+
+a
+  text-decoration: none
+</style>
