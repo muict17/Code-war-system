c00f1ce7ae617ee305a5923e7228d4b7171c3788
Ryan-Willpower <surasak.c.work@gmail.com>
2020-01-11 16:29:27 +0700

    feat: add card component

---
+++ frontend/src/components/CardItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="item">
+    <div class="name">{{ assignmentName }}</div>
+    <div class="score">Max Score: {{ score }} pt</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardItem",
+  props: {
+    assignmentName: String,
+    score: Number,
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.item
+  box-sizing: border-box
+  width: 100%
+  background: #4E5154
+  color: #ACAEAF
+  padding: 30px
+
+.name
+  font-size: 24px
+  margin-bottom: 20px
+</style>
---
+++ frontend/src/components/Cards.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="cards">
+    <CardItem
+      v-for="assignment in assignments"
+      :key="assignment.id"
+      :assignmentName="assignment.name"
+      :score="assignment.score"
+    />
+  </div>
+</template>
+
+<script>
+import CardItem from "./CardItem";
+
+export default {
+  name: "Card",
+  // @todo add api here
+  data: function() {
+    return {
+      assignments: [
+        {
+          id: 0,
+          name: "Title of this assignment",
+          score: 5,
+        },
+        {
+          id: 1,
+          name: "Title of this assignment",
+          score: 5,
+        },
+        {
+          id: 2,
+          name: "Title of this assignment",
+          score: 5,
+        },
+      ],
+    };
+  },
+  components: {
+    CardItem,
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.cards
+  display: grid
+  grid-template-columns: repeat(3, 1fr)
+  grid-gap: 20px
+</style>
