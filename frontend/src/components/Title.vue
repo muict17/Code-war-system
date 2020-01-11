4cf4d60b4ee728360255ba3888e105bd535b6563
Ryan-Willpower <surasak.c.work@gmail.com>
2020-01-11 16:30:07 +0700

    feat: add title component

---
+++ frontend/src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ name }}</h1>
+</template>
+
+<script>
+export default {
+  name: "Title",
+  props: {
+    name: String,
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+h1
+  color: #acadaf
+  margin-top: 40px
+</style>
