116593c93f5b8f068fee74cafc3f00880d071ef7
Ryan-Willpower <surasak.c.work@gmail.com>
2020-01-11 16:29:14 +0700

    feat: add container

---
+++ frontend/src/components/Container.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Container",
+};
+</script>
+
+<style lang="sass" scoped>
+.container
+  margin: 0 120px
+</style>
