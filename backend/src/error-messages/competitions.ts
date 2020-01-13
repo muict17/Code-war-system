17b5c9879676c8416652fb0458d48bb992df0441
lagmanzaza <lagmanmc@gmail.com>
2020-01-13 22:45:15 +0700

    [API, Compeitions] create new

---
+++ backend/src/error-messages/competitions.ts
@@ -0,0 +1,7 @@
+export default [
+  {
+    code: 404,
+    message: `insert or update on table "competitions" violates foreign key constraint "competitions_category_id_fkey"`,
+    appropriateMessage: "Category Id not found"
+  }
+];
